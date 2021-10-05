0879c485b431a4391bd602e7a7f6c3d8479e2e79
ayushshah04 <68362934+ayushshah04@users.noreply.github.com>
2021-10-05 13:27:17 -0500

    update sol file

--- Solidity Hw/Instructions/Starter-Code/DeferredEquityPlan.sol
+++ Solidity Hw/Instructions/Starter-Code/DeferredEquityPlan.sol
@@ -8,12 +8,13 @@ contract DeferredEquityPlan {
     bool active = true; // this employee is active at the start of the contract
 
     // @TODO: Set the total shares and annual distribution
-    // Your code here!
+    uint total_shares = 1000;
+    uint annual_distribution = 250; // 1000 shares for 4 years
 
     uint start_time = now; // permanently store the time this contract was initialized
 
     // @TODO: Set the `unlock_time` to be 365 days from now
-    // Your code here!
+    uint public unlock_time = start_time + 365;
 
     uint public distributed_shares; // starts at 0
 
@@ -28,15 +29,17 @@ contract DeferredEquityPlan {
 
         // @TODO: Add "require" statements to enforce that:
         // 1: `unlock_time` is less than or equal to `now`
+        require(unlock_time <=now, "Shares are not yet vested");
         // 2: `distributed_shares` is less than the `total_shares`
-        // Your code here!
+        require(distributed_shares <= total_shares, "All Shares distributed");
+        
 
         // @TODO: Add 365 days to the `unlock_time`
-        // Your code here!
+        unlock_time +=365;
 
         // @TODO: Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
         // Make sure to include the parenthesis around (now - start_time) to get accurate results!
-        // Your code here!
+        distributed_shares = ((now - start_time) / 365 days * annual_distribution);
 
         // double check in case the employee does not cash out until after 5+ years
         if (distributed_shares > 1000) {
